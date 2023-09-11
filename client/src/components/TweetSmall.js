a61c1928afc8fb3dd51c599c0ab731a1e7b94ec4
Frederick Gitto <frederick_gitto@hotmail.com>
2023-09-11 10:34:25 -0400

    minor image height change

--- client/src/components/TweetSmall.js
+++ client/src/components/TweetSmall.js
@@ -111,7 +111,7 @@ const Status = styled.p`
 
 const Image = styled.img`
   width: 100%;
-  height: 60%;
+  height: 40vh;
   border-radius: 5px;
   object-fit: cover;
   transition: all 200ms ease-in-out;
